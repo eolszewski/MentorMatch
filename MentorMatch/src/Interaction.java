ce6963d4c164b1fee5714e06a6bed30e5ded20a3
Eric Olszewski <eolszewski@gmail.com>
2013-02-23 16:06:49 -0600

    Added Objectify and Further Class work

--- MentorMatch/src/Interaction.java
+++ MentorMatch/src/Interaction.java
@@ -1,4 +1,8 @@
 
 public class Interaction {
-
+	
+	public Interaction()
+	{
+		
+	}
 }
--- MentorMatch/src/User.java
+++ MentorMatch/src/User.java
@@ -1,4 +1,28 @@
+import java.util.ArrayList;
+
 
 public class User {
-
+	
+	private String FirstName, LastName, Email;
+	private ArrayList <String> Interests, Majors, Current_Courses, Past_Courses;
+	
+	public User(String email)
+	{
+		this.Email = email;
+	}
+	
+	public static void LogIn()
+	{
+		
+	}
+	
+	public static void LogOut()
+	{
+		
+	}
+	
+	public static void EditProfile()
+	{
+		
+	}
 }
