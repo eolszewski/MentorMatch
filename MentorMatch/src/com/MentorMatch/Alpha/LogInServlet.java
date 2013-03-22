789937ea1ae96523f1761745f2fd080b38d2b29d
Eric Olszewski <eolszewski@gmail.com>
2013-03-22 14:46:11 -0500

    Added login

--- MentorMatch/src/com/MentorMatch/Alpha/LogInServlet.java
+++ MentorMatch/src/com/MentorMatch/Alpha/LogInServlet.java
@@ -1,5 +1,13 @@
 package com.MentorMatch.Alpha;
 
-public class LogInServlet {
+import java.io.IOException;
+import javax.servlet.http.*;
 
+@SuppressWarnings("serial")
+public class LogInServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
 }
