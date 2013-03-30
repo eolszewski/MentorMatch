079aa9300d9213aa57d5acd9fc2f986c6c006ec6
Eric Olszewski <eolszewski@gmail.com>
2013-03-29 19:16:58 -0500

    Fixed project setup remade login server

--- MentorMatch/src/Servlets/LogInServlet.java
+++ MentorMatch/src/Servlets/LogInServlet.java
@@ -2,46 +2,46 @@ package Servlets;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 
 import Entities.Mentee;
 import Entities.OfyService;
 
 import com.google.gson.Gson;
 
+/*
+import com.google.appengine.labs.repackaged.org.json.JSONObject;
+import com.google.gwt.json.client.JSONValue;*/
+
 @SuppressWarnings("serial")
 public class LogInServlet extends HttpServlet {
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/json");
-		String email = req.getParameter("email");
-		String password = req.getParameter("password");
+		String json = req.getParameter("json");
 		Gson gson = new Gson();
+		Mentee temp = gson.fromJson(json, Mentee.class);
 
-		Mentee fetched = OfyService.ofy().load().type(Mentee.class).id(email).get();
+		Mentee fetched = OfyService.ofy().load().type(Mentee.class).id(temp.getEmail()).get();
 		if(fetched != null)
 		{
-			if(fetched.getPassword().equals(password))
+			if(fetched.getPassword().equals(temp.getPassword()))
 			{
-				req.setAttribute("jsonString", gson.toJson(fetched).toString());
-				resp.sendRedirect(req.getHeader("referer"));
+				req.setAttribute("json", gson.toJson(fetched).toString());
 			}
 			else
 			{
-				req.setAttribute("jsonString", "password");
-				resp.sendRedirect(req.getHeader("referer"));
+				fetched.setPassword(null);
+				req.setAttribute("json", gson.toJson(fetched));
 			}
 		}
 		else
 		{
-			req.setAttribute("jsonString", "password");
-			resp.sendRedirect(req.getHeader("referer"));
+			fetched.setEmail(null);
+			req.setAttribute("json", fetched);
 		}
+		resp.sendRedirect(req.getHeader("referer"));
 	}
 }
