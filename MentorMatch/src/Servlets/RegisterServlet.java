ea659ef1e3db30576847026ad0cb077fbe92c12d
Eric Olszewski <eolszewski@gmail.com>
2013-04-04 16:33:17 -0500

    Refactoring of Register Servlet

--- MentorMatch/src/Servlets/RegisterServlet.java
+++ MentorMatch/src/Servlets/RegisterServlet.java
@@ -24,33 +24,32 @@ public class RegisterServlet extends HttpServlet {
 	
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		Gson gson = new Gson();
-
 		JsonParser jsonParser = new JsonParser();
 		JsonObject jo = (JsonObject)jsonParser.parse(req.getParameter("json"));
 		
-		Mentee temp = new Mentee("eolszewski@gmail.com", " ");
-		OfyService.ofy().delete().entity(temp).now();
+		Mentee temp = new Mentee(" ", " ");
 
 		if(!EmailPassword(jo))
 			temp.setError("No Username and Password input");
 		else if(Exists(jo))
-		{
 			temp.setError("Email is already in use");
-		}
 		else
 		{
 			temp = new Mentee(jo.get("email").toString().replace("\"", ""), jo.get("password").toString().replace("\"", ""));
-			temp.setFirstName(jo.get("firstName").toString().replace("\"", "")).setLastName(jo.get("lastName").toString().replace("\"", "")).setZipCode(Integer.parseInt(jo.get("zipcode").toString().replace("\"", "")));
-			temp.setMajors(Arrays.asList(jo.get("majors").toString().replace("\"", "").replace("[", "").replace("[", "").split(",")));
-			temp.setInterests(Arrays.asList(jo.get("interests").toString().replace("\"", "").replace("[", "").replace("[", "").split(",")));
-			temp.setCurrentCourses(Arrays.asList(jo.get("currentCourses").toString().replace("\"", "").replace("[", "").replace("[", "").split(",")));
-			temp.setPastCourses(Arrays.asList(jo.get("pastCourses").toString().replace("\"", "").replace("[", "").replace("[", "").split(",")));
-			OfyService.ofy().save().entity(temp).now();
+			OfyService.ofy().save().entity(AddFields(temp, jo)).now();
 		}
 		
 		resp.setContentType("application/json");
-		System.out.println(gson.toJson(temp).toString());
 		resp.getWriter().write(gson.toJson(temp).toString());
+	}
+
+	private Object AddFields(Mentee temp, JsonObject jo) {
+		temp.setFirstName(jo.get("firstName").toString().replace("\"", "")).setLastName(jo.get("lastName").toString().replace("\"", "")).setZipCode(Integer.parseInt(jo.get("zipcode").toString().replace("\"", "")));
+		temp.setMajors(Arrays.asList(jo.get("majors").toString().replace("\"", "").replace("[", "").replace("[", "").split(",")));
+		temp.setInterests(Arrays.asList(jo.get("interests").toString().replace("\"", "").replace("[", "").replace("[", "").split(",")));
+		temp.setCurrentCourses(Arrays.asList(jo.get("currentCourses").toString().replace("\"", "").replace("[", "").replace("[", "").split(",")));
+		temp.setPastCourses(Arrays.asList(jo.get("pastCourses").toString().replace("\"", "").replace("[", "").replace("[", "").split(",")));
+		return temp;
 	}
 
 	private boolean EmailPassword(JsonObject jo) {
