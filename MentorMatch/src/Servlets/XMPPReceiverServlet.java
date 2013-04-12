96f5bae4358a4c4491bdb0595ac2c9c80dc99f41
Asad S Malik <asadsm@utexas.edu>
2013-04-11 19:05:11 -0500

    Added storage capability to received message

--- MentorMatch/src/Servlets/XMPPReceiverServlet.java
+++ MentorMatch/src/Servlets/XMPPReceiverServlet.java
@@ -1,21 +1,42 @@
 package Servlets;
 import java.io.IOException;
+import java.util.Date;
+
 import javax.servlet.http.*;
+
+import Entities.OfyService;
+
+import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.xmpp.JID;
 import com.google.appengine.api.xmpp.Message;
 import com.google.appengine.api.xmpp.XMPPService;
 import com.google.appengine.api.xmpp.XMPPServiceFactory;
+
 
 @SuppressWarnings("serial")
 public class XMPPReceiverServlet extends HttpServlet {
 	
     public void doPost(HttpServletRequest req, HttpServletResponse res)
           throws IOException {
+    	
         XMPPService xmpp = XMPPServiceFactory.getXMPPService();
         Message message = xmpp.parseMessage(req);
         
-        JID fromJid = message.getFromJid();
-        String body = message.getBody();
+        Entity entity = new Entity("Message");
+        
+        StringBuilder to = new StringBuilder();
+        
+        for(JID jid: message.getRecipientJids()){
+        	to.append(jid.toString());
+        	to.append("; ");
+        }
+        
+        entity.setProperty("from", "" + message.getFromJid());
+        entity.setProperty("body", message.getBody());
+        entity.setProperty("Timestamp", new Date());
+        
+        
+        OfyService.ofy().save().entity(entity).now();
         
         xmpp.sendMessage(message);
         // ...
--- MentorMatch/src/Tests/TestXmppServlet.java
+++ MentorMatch/src/Tests/TestXmppServlet.java
@@ -15,15 +15,15 @@ public class TestXmppServlet{
         HttpServletRequest request = mock(HttpServletRequest.class);       
         HttpServletResponse response = mock(HttpServletResponse.class);    
 
-        when(request.getParameter("username")).thenReturn("me");
-        when(request.getParameter("password")).thenReturn("secret");
+        when(request.getParameter("username")).thenReturn("username");
+        when(request.getParameter("password")).thenReturn("password");
         PrintWriter writer = new PrintWriter("somefile.txt");
-        when(response.getWriter()).thenReturn(writer);
+       when(response.getWriter()).thenReturn(writer);
 
         new XMPPReceiverServlet().doPost(request, response);
 
         verify(request, atLeast(1)).getParameter("username"); // only if you want to verify username was called...
-        writer.flush(); // it may not have been flushed yet...
+       writer.flush(); // it may not have been flushed yet...
         
     }
     
