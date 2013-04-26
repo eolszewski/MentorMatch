ef305a0e4515c50c222c453190d5f24c56d80a8e
Eric Olszewski <eolszewski@gmail.com>
2013-04-26 14:37:42 -0500

    EditProfile Servlet

---
+++ MentorMatch/src/Servlets/EditServlet.java
@@ -0,0 +1,43 @@
+package Servlets;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Entities.Mentee;
+import Entities.OfyService;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class EditServlet extends HttpServlet {
+	Gson gson = new Gson();
+	
+	boolean TEST = false;
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {		
+					
+		JsonParser jsonParser = new JsonParser();
+		JsonObject jo = (JsonObject)jsonParser.parse(req.getParameter("json"));
+		Mentee fetched;
+		
+		resp.setContentType("text/plain");
+		if(jo.get("flag").toString().replace("\"", "").equals("fill"))
+		{
+			fetched =  OfyService.ofy().load().type(Mentee.class).id(jo.get("email").toString().replace("\"", "")).get();
+		}
+		else
+		{
+			fetched = new Mentee("null", 0);
+		}
+		resp.getWriter().write(gson.toJson(fetched).toString());
+	}
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {			
+		doPost( req, resp );
+	}
+}
