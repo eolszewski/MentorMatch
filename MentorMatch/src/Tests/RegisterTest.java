e0398cfdb517c1527ec51aaa9f1be5c49ecfd9b5
Asad S Malik <asadsm@utexas.edu>
2013-04-29 23:34:37 -0500

    Finalized Registering Test

--- MentorMatch/src/Tests/RegisterTest.java
+++ MentorMatch/src/Tests/RegisterTest.java
@@ -9,70 +9,69 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class RegisterTest  {
     public static void main(String[] args) {
-        // Create a new instance of the Firefox driver
-        // Notice that the remainder of the code relies on the interface, 
-        // not the implementation.
+       
+    	
+    	
+    	String first_User = "emailReg,eric@gmail.com,passwordReg,123456,fname,Eric,lname,Olz,zip,78705";
+		String first_Details = "majors,Electrical Engineering,majorb,interests,Football,interestb,currentcourses,EE 461L,ccourseb,pastcourses,ASE 201,pcourseb";
+
+		String second_User = "emailReg,asad@gmail.com,passwordReg,123456,fname,Asad,lname,Malik,zip,78705";
+		String second_Details = "majors,Electrical Engineering,majorb,interests,Soccer,interestb,currentcourses,EE 461L,ccourseb,pastcourses,ASE 201,pcourseb";
+
+		
         WebDriver driver = new FirefoxDriver();
-
-        // And now use this to visit Google
         driver.get("http://localhost:8888/");
-        // Alternatively the same thing can be done like this
-        // driver.navigate().to("http://www.google.com");
+        
 
-        // Find the text input element by its name
         WebElement element = driver.findElement(By.id("register"));
-
-        // Enter something to search for
-       // element.sendKeys("Cheese!");
-
-        // Now submit the form. WebDriver will find the form for us from the element
-       // element.submit();
-        element.click();
+		element.click();
+		
+	
+		createUser(element,driver,first_User,first_Details);
         
-        element = driver.findElement((By.id("emailReg")));
-        element.sendKeys("john@gmail.com");
-        element = driver.findElement((By.id("passwordReg")));
-        element.sendKeys("123456");
-        element = driver.findElement((By.id("fname")));
-        element.sendKeys("John");
-        element = driver.findElement((By.id("lname")));
-        element.sendKeys("Doe");
-        element = driver.findElement((By.id("zip")));
-        element.sendKeys("78705");
-        element = driver.findElement((By.id("majors")));
-        element.sendKeys("Electrical Engineering");
-        element = driver.findElement((By.id("majorb")));
-        element.click();
-        element = driver.findElement((By.id("interests")));
-        element.sendKeys("Soccer");
-        element = driver.findElement((By.id("interestb")));
-        element.click();
-        element = driver.findElement((By.id("currentcourses")));
-        element.sendKeys("EE 461L");
-        element = driver.findElement((By.id("ccourseb")));
-        element.click();
-        element = driver.findElement((By.id("pastcourses")));
-        element.sendKeys("ASE 201");
-        element = driver.findElement((By.id("pcourseb")));
-        element.click();
-        element = driver.findElement((By.id("submitb")));
-        element.click();
-        
-        // Check the title of the page
-        System.out.println("Page title is: " + driver.getTitle());
-        
-        // Google's search is rendered dynamically with JavaScript.
-        // Wait for the page to load, timeout after 10 seconds
-        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver d) {
-                return d.getTitle().toLowerCase().startsWith("cheese!");
-            }
-        });
-
-        // Should see: "cheese! - Google Search"
-        System.out.println("Page title is: " + driver.getTitle());
-        
+		/* WebDriver driver2 = new FirefoxDriver();
+	     driver2.get("http://localhost:8888/");
+	        
+	    WebElement element2 = driver2.findElement(By.id("register"));
+		element.click();
+			
+		createUser(element2,driver2,second_User,second_Details);
+		
+		*/
+		
+		
         //Close the browser
         driver.quit();
     }
+    
+    
+    public static void createUser(WebElement element,  WebDriver driver, String User, String Details){
+    	String [] first = User.split(",");
+    	String [] fDetails = Details.split(",");
+    	
+    	
+    	 for(int i = 0; i < first.length-1; i++){
+
+         	element = driver.findElement(By.id(first[i]));
+         	element.sendKeys(first[++i]);
+         	
+         }
+         
+         for(int i = 0; i < fDetails.length-2; i++){
+
+         	element = driver.findElement(By.id(fDetails[i]));
+         	element.sendKeys(fDetails[++i]);
+         	element = driver.findElement(By.id(fDetails[++i]));
+             element.click();
+
+         	
+         }
+         
+
+         element = driver.findElement((By.id("submitb")));
+         element.click();
+    }
+    
+    
+    
 }
